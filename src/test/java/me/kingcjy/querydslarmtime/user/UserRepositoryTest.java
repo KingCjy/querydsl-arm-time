7c27c01cc694f109a275080eb09f62cb7322e94e
KingCjy <tlssycks@gmail.com>
2022-08-25 20:17:49 +0900

    test: test

--- src/test/java/me/kingcjy/querydslarmtime/user/UserRepositoryTest.java
+++ src/test/java/me/kingcjy/querydslarmtime/user/UserRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import javax.persistence.EntityManager;
@@ -18,7 +19,7 @@ import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
+@SpringBootTest
 @ActiveProfiles("test")
 class UserRepositoryTest {
 
