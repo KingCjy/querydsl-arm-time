0edef3d6458c665bcbd78c9b4a0c8ddf8e76fe5d
KingCjy <tlssycks@gmail.com>
2022-08-25 17:15:54 +0900

    test: test

--- src/test/java/me/kingcjy/querydslarmtime/user/UserRepositoryTest.java
+++ src/test/java/me/kingcjy/querydslarmtime/user/UserRepositoryTest.java
@@ -2,14 +2,19 @@ package me.kingcjy.querydslarmtime.user;
 
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.hibernate.Session;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
+
+import java.sql.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,6 +30,9 @@ class UserRepositoryTest {
 
     private JPAQueryFactory jpaQueryFactory;
 
+    @Autowired
+    private DataSource dataSource;
+
     @BeforeEach
     void before() {
         jpaQueryFactory = new JPAQueryFactory(entityManager);
@@ -32,7 +40,7 @@ class UserRepositoryTest {
     }
 
     @Test
-    void test() {
+    void test() throws ClassNotFoundException, SQLException {
         User user = userRepository.findById(1L).get();
         UserDto userDto1 = new UserDto(user.getId(), user.getName(), user.getCreatedAt());
         UserDto userDto2 = jpaQueryFactory.select(Projections.constructor(UserDto.class, QUser.user.id, QUser.user.name, QUser.user.createdAt))
@@ -42,6 +50,19 @@ class UserRepositoryTest {
         System.out.println(userDto1);
         System.out.println(userDto2);
 
+        Session session = entityManager.unwrap(Session.class);
+        session.doWork(connection ->{
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from \"user\" user0_");
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if(resultSet.next()) {
+                Timestamp timestamp = resultSet.getTimestamp(2);
+                System.out.println("jdbc driver timestamp: " + timestamp);
+            }
+        });
+
         assertEquals(userDto1, userDto2);
     }
+
+
 }
