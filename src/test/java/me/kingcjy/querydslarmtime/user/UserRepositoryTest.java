6572e502c6209eb0baf561da8aafc3d89dfa314a
KingCjy <tlssycks@gmail.com>
2022-08-25 20:13:17 +0900

    test: test

--- src/test/java/me/kingcjy/querydslarmtime/user/UserRepositoryTest.java
+++ src/test/java/me/kingcjy/querydslarmtime/user/UserRepositoryTest.java
@@ -7,10 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -21,8 +18,7 @@ import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-//@Import(TestConfig.class)
+@DataJpaTest
 @ActiveProfiles("test")
 class UserRepositoryTest {
 
